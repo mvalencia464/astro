7c83db7cce861253e4d09ae279ab98ba8a176de0
mvalencia464 <mauricio@stokeleads.com>
2026-02-21 22:00:25 -0800

    astro ready for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,32 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+    platformProxy: {
+      enabled: true,
+    },
+  }),
   integrations: [react()],
   image: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.stokeleads.com',
-        port: undefined,
-        pathname: '/**',
       },
     ],
   },
-  vite: {
-    plugins: [tailwindcss()], // No extra options needed here
+vite: {
+    plugins: [tailwindcss()],
+    build: {
+      target: 'esnext',
+      cssMinify: 'esbuild', // Change from 'lightningcss' to 'esbuild'
+    },
     ssr: {
       external: ['lucide-react'],
     },
-  },
-});
+  },});
